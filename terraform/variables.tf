77bbc630077e9dbeda6ef76ce9b5487605dd3534
grossj0816 <juwangross0815@gmail.com>
2025-09-01 09:59:03 -0400

    reverted back to old way of storing networking credentials

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,22 @@
 variable "public_subnet_cidrs" {
   type      = list(string)
-  sensitive = true
+  default = ["10.0.7.0/24", "10.0.8.0/24"]
 }
 
 variable "lambda_subnet_cidrs" {
   type      = list(string)
-  sensitive = true
+  default = ["10.0.9.0/24", "10.0.10.0/24"]
 }
 
 variable "database_subnet_cidrs" {
   type      = list(string)
-  sensitive = true
+  sensitive = ["10.0.11.0/24", "10.0.12.0/24"]
+
 }
 
 variable "azs" {
   type      = list(string)
-  sensitive = true
+  default = ["us-east-1d", "us-east-1e", "us-east-1f"]
 }
 
 variable "username" {
